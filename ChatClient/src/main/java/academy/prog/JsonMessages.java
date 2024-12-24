549a691015d4f051812976b0c24046605b1cd0e6
Roman Manzhula <roman1985al@ukr.net>
2024-12-24 22:03:42 +0200

    Update JsonMessages.java
    In Ukrainian

--- ChatClient/src/main/java/academy/prog/JsonMessages.java
+++ ChatClient/src/main/java/academy/prog/JsonMessages.java
@@ -5,20 +5,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class JsonMessages {
-    private final List<Message> list = new ArrayList<>(); //список куда копируем сообщения
+    private final List<Message> list = new ArrayList<>(); //список, в який копіюємо повідомлення
 
-    public JsonMessages(List<Message> sourceList, int fromIndex, String to) { //метод для копирования
-        // сообщений из списка от указанного индекса и до конца (параметры: список из которого копируем, индекс - с какого
-        // сообщения начинаем копировать, строка логин получателя(адрес))
-        for (int i = fromIndex; i < sourceList.size(); i++) { //проходим по списку от fromIndex и до конца
-            if (sourceList.get(i).getTo().equals(to)) { //если элемент списка по полученному отправителю (getTo())
-                //будет = адресату, то
-                list.add(sourceList.get(i)); //добавляем этот элемент в список
+    public JsonMessages(List<Message> sourceList, int fromIndex, String to) { //метод для копіювання
+        // повідомлень зі списку, починаючи з вказаного індексу і до кінця (параметри: список, з якого копіюємо, індекс - з якого
+        // повідомлення починаємо копіювати, рядок логіну отримувача (адреса))
+        for (int i = fromIndex; i < sourceList.size(); i++) { //проходимо по списку з fromIndex до кінця
+            if (sourceList.get(i).getTo().equals(to)) { //якщо елемент списку за отримувачем (getTo())
+                //буде = адресату, то
+                list.add(sourceList.get(i)); //додаємо цей елемент до списку
             }
         }
     }
 
-    public List<Message> getList() { //метод возвращает неизменяемый(только для чтения) список суперкласса Collections
+    public List<Message> getList() { //метод повертає незмінний (тільки для читання) список суперкласу Collections
         return Collections.unmodifiableList(list);
     }
 }
